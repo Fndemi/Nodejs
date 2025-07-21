3412c9a9ebd20e0cae0e80e01af80cebe23931bf
Florence Ndemi <ndemiflorence@gmail.com>
2025-07-21 20:14:21 +0300

    Commit:Learnt about event emitter

--- chapter3/index.js
+++ chapter3/index.js
@@ -1,5 +1,4 @@
 const { format } = require('date-fns');
 const { v4: uuid } = require('uuid');
 console.log(format(new Date(), 'yyyy-MM-dd HH:mm:ss')) // Outputs the current date and time in a formatted string
-
 console.log(uuid())
