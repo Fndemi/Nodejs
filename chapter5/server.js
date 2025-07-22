2efce8d1397d789d0aaa60be85123b9210c6ce63
Florence Ndemi <ndemiflorence@gmail.com>
2025-07-22 06:48:04 +0300

    Commit:completed web server

--- chapter5/server.js
+++ chapter5/server.js
@@ -11,27 +11,41 @@ class Emitter extends EventEmitter { }
 
 const myEmitter = new Emitter();
 
+myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
 
 const PORT = process.env.PORT || 3500;
 
 
 const serveFile = async (filePath, contentType, response) => {
   try {
-    const data = await fsPromises.readFile(filePath, 'utf8');
-    response.writeHead(200, { 'Content-Type': contentType });
-    response.end(data);
+    const rawData = await fsPromises.readFile(
+      filePath,
+      !contentType.includes('image') ? 'utf8' : ''
+
+    );
+    const data = contentType === 'application/json' ? JSON.parse(rawData) : rawData;
+    response.writeHead(
+      filePath.includes('404.html') ? 404 : 200,
+      { 'Content-Type': contentType });
+    response.end(
+      contentType === 'application/json' ? JSON.stringify(data) : data
+    );
   }
   catch (err) {
     console.log(err);
+    myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt');
+
     response.statusCode = 500;
     response.end();
   }
 }
 
-
 const server = http.createServer((req, res) => {
 
   console.log(req.url, req.method);
+
+  myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt');
+
   const extension = path.extname(req.url);
   let contentType;
 
@@ -70,7 +84,6 @@ const server = http.createServer((req, res) => {
   if (!extension && req.url.slice(-1) !== '/')
     filePath += '.html';
 
-
   const fileExists = fs.existsSync(filePath);
   if (fileExists)
     serveFile(filePath, contentType, res);
@@ -86,7 +99,7 @@ const server = http.createServer((req, res) => {
         break;
       default:
         //serve a 404 response
-        serveFile(path.join(__dirname, 'views', '404.html'), contentType, res);
+        serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
 
     };
   }
@@ -99,9 +112,3 @@ server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 
-//add listener for the log event
-// myEmitter.on('log', (msg) => logEvents(msg));
-// setTimeout(() => {
-//   //Emit event
-//   myEmitter.emit('log', 'Log event emitted!')
-// }, 2000);
