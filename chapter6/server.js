a28bc780f9ff844e51fd980d73079e56851b99b1
Florence Ndemi <ndemiflorence@gmail.com>
2025-07-22 16:23:34 +0300

    Commit:Added route handlers

--- chapter6/server.js
+++ chapter6/server.js
@@ -14,19 +14,39 @@ app.get(/^\/old-page(.html)?$/, (req, res) => {
   res.redirect(301, '/new-page.html'); //302 by default
 });
 
+
+//Route handlers
+app.get(/^\/hello(.html)?$/, (req, res, next) => {
+  console.log('attempted to load hello.html');
+  next()
+}, (re, res) => {
+  res.send('<h1>Hello World!</h1>');
+});
+
+//chaining route handlers
+
+const one = (req, res, next) => {
+  console.log('one');
+  next();
+}
+
+const two = (req, res, next) => {
+  console.log('two');
+  next();
+}
+
+const three = (req, res) => {
+  console.log('three');
+  res.send('Finished!');
+}
+
+app.get(/^\/chain(.html)?$/, [one, two, three]);
+
+
+
 app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
-
-//Route handlers
-app.get('/hello(.html)?', (req, res, next) => {
-  console.log('attempted to load hello.html');
-  next()
-});
-
-
-
-
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
