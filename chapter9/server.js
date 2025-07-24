7618f18e6f8a7ece85c8bd6f0c5fe50d873b75fa
Florence Ndemi <ndemiflorence@gmail.com>
2025-07-24 14:30:07 +0300

    Commit:used MVC REST APU

--- chapter9/server.js
+++ chapter9/server.js
@@ -2,27 +2,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const corsOptions = require('./config/corsOptions'); // import CORS options
 const errorHandler = require('./middleware/errorHandler');
 const { logger } = require('./middleware/logEvents');
+
 const PORT = process.env.PORT || 3500;
 // custom middleware logger
 app.use(logger);
 //Cross origin Resurce Sharing
-const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1.5500', 'http://localhost:3500'];
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1 || !origin) { // allow requests with no origin like mobile apps or curl requests
-      callback(null, true);
-    }
-    else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
 app.use(cors(corsOptions)); // enable CORS for all requests
-
 //built-in middleware t habdle urlencoded data
 //in other words, form data
 //content-type: application/x-www-form-urlencoded
@@ -33,11 +22,9 @@ app.use(express.json());
 
 //serve static files
 app.use(express.static(path.join(__dirname, '/public')));
-app.use("/subdir", express.static(path.join(__dirname, '/public')));
 
 //routes
 app.use('/', require('./routes/root'));
-app.use('/subdir', require('./routes/subdir'));
 app.use('/employees', require('./routes/api/employees'));
 
 app.all("*", (req, res) => {
